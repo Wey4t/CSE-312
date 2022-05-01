cb3f800b65ab4c916faf01613ff6400047629c10
Jiawei Wu <89705390+Wey4t@users.noreply.github.com>
2022-05-01 17:21:08 -0400

    Update chat.js

--- src/staticFile/js/chat.js
+++ src/staticFile/js/chat.js
@@ -1,23 +1,15 @@
 function select_user(user) {
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            var user_list = document.getElementsByClassName(user.className)
-            for(let i = 0; i < user_list.length;++i){
-                var u = user_list[i]
-                u.className = u.className.replace('active',' ');
-            //console.log(u.className)
-            }
-            if (user.className.includes('active')) {
-                user.className = user.className.replace('active',' ');
-            }else{
-                user.className += 'active'
-            }
-        }
+    window.location.href='/chat-history/'+user.id    
+}
+function acitve_user() {
+    let posistion = window.location.href.lastIndexOf('/')
+    let username =  window.location.href.slice(posistion+1)
+    console.log(username)
+    let user = document.getElementById(username)
+    if (user.className.includes('active')) {
+        user.className = user.className.replace('active',' ');
+    }else{
+        user.className += ' active'
+    }
         
-    };
-    var name = user.id
-    request.open("GET", "/chat-history/"+name);
-    request.send();
-    
 }

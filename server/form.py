2d26cffc3f68eb1d40c5b480f629dee3fcaf6115
jiawei <jwu235@buffalo.edu>
2022-05-10 15:02:57 -0400

    update form class

--- server/form.py
+++ server/form.py
@@ -1,31 +1,14 @@
 from request import *
 class Form:
-    def __init__(self, request, names):
-        self.table={}
-        if first_form_name(request.body) in names:
-            self.table[first_form_name(request.body)] = firstValue(request.body)
-        if second_form_name(request.body) in names:
-            self.table[second_form_name(request.body)] = secondValue(request.body)
-        else:
-            return 
-
-def first_form_name(data:bytes):
-    form_name = data.split(Request.blank_line)[0]
-    form_name = form_name[form_name.find(b'name='):].split(b'"')[1]
-    return form_name.decode()
-
-def second_form_name(data: bytes):
-    form_name = data.split(Request.blank_line)[1]
-    form_name = form_name[form_name.find(b'name='):].split(b'"')[1]
-    return form_name.decode()
-
-def firstValue(data: bytes):
-    username=data.split(Request.blank_line)[1].split(Request.new_line)[0]
-    return username.decode()
-
-def secondValue(data: bytes):
-    password = data.split(Request.blank_line)[2].split(Request.new_line)[0]
-    return password.decode()
+    def __init__(self, request:Request, keys):
+        self.talbe = {}
+        print(request.body)
+        contents = request.body.split(request.boundary)[1:-1]
+        pair = zip(contents, keys)
+        for content, key in pair:
+            content = content[:-2]
+            data = content[content.find(b'\r\n\r\n')+len(b'\r\n\r\n'):]
+            self.talbe[key] = data
 
 if __name__ == "__main__":
     test_case_1 = b'POST /sign-in HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nContent-Length: 256\r\nCache-Control: max-age=0\r\nsec-ch-ua: " Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"\r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: "Windows"\r\nUpgrade-Insecure-Requests: 1\r\nOrigin: http://localhost:8080\r\nContent-Type: multipart/form-data; boundary=----WebKitFormBoundaryZ1WXYkJKPvmf4Bap\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: same-origin\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nReferer: http://localhost:8080/SignIn.html\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7\r\n\r\n------WebKitFormBoundaryZ1WXYkJKPvmf4Bap\r\nContent-Disposition: form-data; name="user_name"\r\n\r\nTom Aan\r\n------WebKitFormBoundaryZ1WXYkJKPvmf4Bap\r\nContent-Disposition: form-data; name="user_password"\r\n\r\nZSZDaawu!\r\n------WebKitFormBoundaryZ1WXYkJKPvmf4Bap--\r\n'

e0b78bfe909b1beb5a9afa6e3adddbbfc0caeef0
jiawei <jwu235@buffalo.edu>
2022-05-13 15:11:20 -0400

    add profile noti

--- src/staticFile/js/ws.js
+++ src/staticFile/js/ws.js
@@ -1,18 +1,9 @@
 const socket = new WebSocket('ws://' + window.location.host + '/websocket');
 setInterval(ping, 200)
 function ping(){
-    socket.send(JSON.stringify({"type": "ping"}));
+    socket.send(JSON.stringify({"type": "chat_ping"}));
     }
 
-socket.onmessage = function (ws_message) {
-    const message = JSON.parse(ws_message.data);
-    const messageType = message.type
-    if (messageType === 'pong'){
-        console.log('reciv')
-        var a = document.getElementById('noti')
-        a.hidden = false
-    }
-}
  socket.onmessage = function (ws_message) {
      const message = JSON.parse(ws_message.data);
      const messageType = message.type
